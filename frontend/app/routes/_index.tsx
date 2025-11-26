5909774a87217de84fcc33ea3bf3da08666f60c5
Samuel Melander <sammel-2@student.ltu.se>
2025-11-26 10:00:01 +0100

    cicd test

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router";
 export default function LandingPage() {
     return(
         <div className="p-10">
-            <h1>Index pagess</h1>
+            <h1>Index pagess, Hej nu är den uppdaterad</h1>
             <NavLink to="/login">Login Page</NavLink>
             <br/>
             <NavLink to="/signup">Signup Page</NavLink>
