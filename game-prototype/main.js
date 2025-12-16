ddfe925b8bf5ad508469490fc80969bdb46f21d8
Samuel <cool23man23@gmail.com>
2025-12-16 22:27:30 +0100

    fixed some cors issues

--- game-prototype/main.js
+++ game-prototype/main.js
@@ -3,17 +3,19 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const { Server } = require("socket.io")
+const http = require("http")
+const server = http.createServer(app);
 
 const USER1 = process.env["USER1"]
 const USER2 = process.env["USER2"]
 const MATCH_ID = process.env["MATCH_ID"]
 
-if(!USER1 || !USER2) {
+if (!USER1 || !USER2) {
     console.error("User1 or User2 environment variables not set. Make sure they are identical to JWT.sub.")
     process.exit(1)
 }
 
-if(!MATCH_ID) {
+if (!MATCH_ID) {
     console.error("Match doesnt have an ID. Remember to set the environment variable MATCH_ID.1")
     process.exit(1)
 }
@@ -23,11 +25,13 @@ const { rabbitmq } = require("./rabbit")
 
 app.use(cors())
 
+app.get("/health", (req, res) => {
+    res.send("Alive")
+})
+
 const PORT = parseInt(process.env["PORT"] || "3000")
 
-const server = app.listen(PORT, () => {
-    console.log(`Game Server listening on port ${PORT}\nWaiting for users: \n\t${USER1} \n\t${USER2}`)
-})
+
 
 function verifyJwt(token) {
     return {
@@ -39,7 +43,7 @@ const publisher = rabbitmq.createPublisher({
     confirm: true,
     maxAttempts: 5,
     exchanges: [{ exchange: "match-events", type: "topic" }],
-    
+
 })
 
 game.init(USER1, USER2)
@@ -58,13 +62,17 @@ game.onGameOver(winner => {
 
 const io = new Server(server, {
     cors: {
-        origin: "*"
+        origin: [
+            "http://localhost:5173",
+            "https://ltu-m7011e-1.se"
+        ],
+        credentials: true,
     }
 })
 
 io.use((socket, next) => {
     const jwt = socket.handshake.auth.token;
-    if(!jwt) {
+    if (!jwt) {
         return next(new Error("Unauthorized"))
     }
 
@@ -72,7 +80,7 @@ io.use((socket, next) => {
         const payload = verifyJwt(jwt)
         const id = payload.sub
 
-        if(!(id === USER1 ||id === USER2)) {
+        if (!(id === USER1 || id === USER2)) {
             return next(new Error("You aren't apart of this match."))
         }
 
@@ -85,6 +93,9 @@ io.use((socket, next) => {
 
 io.on("connection", (socket) => game.socketConnection(io, socket))
 
+server.listen(PORT, () => {
+    console.log(`Game Server listening on port ${PORT}\nWaiting for users: \n\t${USER1} \n\t${USER2}`)
+})
 
 async function onShutdown() {
     await publisher.close()
