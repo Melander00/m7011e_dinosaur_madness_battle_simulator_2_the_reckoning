b0a9491f1b5898f640b0fb0a9b6f04bd3673cd19
irmaph <156810498+irmaph@users.noreply.github.com>
2026-01-05 10:34:55 +0100

    mellanrum för ja retard

--- frontend/keycloak/keycloak.ts
+++ frontend/keycloak/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
   url: "https://keycloak.ltu-m7011e-1.se",
   realm: "myapp",
-  clientId: "my-frontend-app`",
+  clientId: "my-frontend-app",
 });
 
 export default keycloak;
