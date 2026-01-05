1f88fac9d379bbd0c23897a5c9b844d93a049ed3
Samuel <cool23man23@gmail.com>
2026-01-05 13:45:43 +0100

    ping

--- ping/main.js
+++ ping/main.js
@@ -29,7 +29,7 @@ app.get("/metrics", async (req, res) => {
     res.end(await register.metrics())
 })
 
-app.get("/api/ping", (req, res) => {
+app.get("/", (req, res) => {
 
     const start = Date.now()
 
