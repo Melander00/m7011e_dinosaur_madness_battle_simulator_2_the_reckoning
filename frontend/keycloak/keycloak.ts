8662a342c5e808500bd4937e1f58afb2352ba18c
irmaph <156810498+irmaph@users.noreply.github.com>
2026-01-05 10:34:22 +0100

    Update Keycloak clientId to 'my-frontend-app'
    please

--- frontend/keycloak/keycloak.ts
+++ frontend/keycloak/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
   url: "https://keycloak.ltu-m7011e-1.se",
   realm: "myapp",
-  clientId: "frontend",
+  clientId: "my-frontend-app`",
 });
 
 export default keycloak;
