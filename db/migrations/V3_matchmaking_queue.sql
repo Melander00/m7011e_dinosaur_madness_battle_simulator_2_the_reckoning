745b79dd64891508741d685e321cdd885a44ad2c
Samuel <cool23man23@gmail.com>
2026-01-06 09:44:37 +0100

    removed abundant migration file

--- db/migrations/V3_matchmaking_queue.sql
+++
@@ -1,6 +0,0 @@
--- V2__matchmaking_queue.sql
-CREATE TABLE matchmaking_queue (
-    userId UUID PRIMARY KEY REFERENCES users(userId) ON DELETE CASCADE,
-    elo INTEGER NOT NULL DEFAULT 1000,
-    queue_start_time TIMESTAMP NOT NULL DEFAULT NOW()
-);
